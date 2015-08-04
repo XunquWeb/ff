38c3f57f1db51af1fc907d5c94ac6c0fa36aaae6
cao123 <caozhechao@126.com>
2015-08-04 20:20:49 +0800

    database update

--- xunquweb.sql
+++ xunquweb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2015 年 07 月 14 日 22:15
+-- 生成日期: 2015 年 08 月 04 日 20:20
 -- 服务器版本: 5.6.12-log
 -- PHP 版本: 5.4.12
 
@@ -36,6 +36,7 @@ CREATE TABLE IF NOT EXISTS `act` (
   `s_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `extra` varchar(255) NOT NULL DEFAULT ' ',
   `t_id` int(10) NOT NULL,
+  `a_state` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=37 ;
 
@@ -43,43 +44,43 @@ CREATE TABLE IF NOT EXISTS `act` (
 -- 转存表中的数据 `act`
 --
 
-INSERT INTO `act` (`id`, `u_id`, `name`, `j_time`, `s_time`, `extra`, `t_id`) VALUES
-(1, 1, 'running', '2015-05-04 12:00:00', '2015-05-05 02:00:00', 'everyone is ok', 1),
-(2, 1, 'running2', '2015-05-04 12:00:00', '2015-05-05 02:00:00', 'everyone is ok', 1),
-(3, 1, 'running23', '2015-05-04 12:00:00', '2015-05-05 02:00:00', 'everyone is ok', 1),
-(4, 1, 'running234', '2015-05-04 12:00:00', '2015-05-05 02:00:00', 'everyone is ok', 1),
-(5, 1, 'running2345', '2015-05-04 12:00:00', '2015-05-05 02:00:00', 'everyone is ok', 1),
-(6, 1, 'running23456', '2015-05-04 12:00:00', '2015-05-05 02:00:00', 'everyone is ok', 1),
-(7, 1, 'running234567', '2015-05-04 12:00:00', '2015-06-04 15:25:58', 'everyone is ok', 2),
-(8, 1, 'running2345678', '2015-05-04 12:00:00', '2015-06-04 15:25:30', 'everyone is ok', 2),
-(9, 1, 'running23456789', '2015-05-04 12:00:00', '2015-06-04 15:25:35', 'everyone is ok', 3),
-(10, 1, 'running234567890', '2015-05-04 12:00:00', '2015-06-04 15:25:40', 'everyone is ok', 7),
-(11, 1, 'wtf', '2015-05-04 12:00:00', '2015-05-05 02:00:00', 'everyone is ok', 1),
-(12, 1, 'wtf1', '2015-05-04 12:00:00', '2015-05-05 02:00:00', 'everyone is ok', 1),
-(13, 1, 'wtf12', '2015-05-04 12:00:00', '2015-06-04 15:25:47', 'everyone is ok', 5),
-(14, 1, 'wtf123', '2015-05-04 12:00:00', '2015-05-05 02:00:00', 'everyone is ok', 1),
-(15, 1, 'wtf1234', '2015-05-04 12:00:00', '2015-05-05 02:00:00', 'everyone is ok', 1),
-(16, 1, 'wtf12345', '2015-05-04 12:00:00', '2015-05-05 02:00:00', 'everyone is ok', 1),
-(17, 2, 'study', '0000-00-00 00:00:00', '2015-06-04 15:22:06', '', 7),
-(18, 2, 'studyy', '0000-00-00 00:00:00', '2015-06-04 15:22:40', '', 7),
-(19, 2, 'studyyy', '0000-00-00 00:00:00', '2015-06-04 15:22:40', '', 7),
-(20, 2, 'studyyyy', '0000-00-00 00:00:00', '2015-06-04 15:22:40', '', 7),
-(21, 2, 'studyyyyy', '0000-00-00 00:00:00', '2015-06-04 15:22:41', '', 7),
-(22, 2, 'studyyyyyy', '0000-00-00 00:00:00', '2015-06-04 15:22:41', '', 7),
-(23, 2, 'studys', '0000-00-00 00:00:00', '2015-06-04 15:22:41', '', 7),
-(24, 2, 'studyss', '0000-00-00 00:00:00', '2015-06-04 15:22:41', '', 7),
-(25, 2, 'studysss', '0000-00-00 00:00:00', '2015-06-04 15:23:38', '', 7),
-(26, 2, 'studyssss', '0000-00-00 00:00:00', '2015-06-04 15:23:38', '', 7),
-(27, 2, 'studysssss', '0000-00-00 00:00:00', '2015-06-04 15:23:38', '', 7),
-(28, 2, 'studyssssss', '0000-00-00 00:00:00', '2015-06-04 15:23:38', '', 7),
-(29, 2, 'study1', '0000-00-00 00:00:00', '2015-06-04 15:23:38', '', 7),
-(30, 2, 'study11', '0000-00-00 00:00:00', '2015-06-04 15:23:38', '', 7),
-(31, 2, 'study111', '0000-00-00 00:00:00', '2015-06-04 15:23:38', '', 7),
-(32, 2, 'study1111', '0000-00-00 00:00:00', '2015-06-04 15:23:38', '', 7),
-(33, 2, 'lalala', '0000-00-00 00:00:00', '2015-06-04 15:24:46', '', 2),
-(34, 2, 'bobobo', '0000-00-00 00:00:00', '2015-06-04 15:24:46', '', 3),
-(35, 2, 'labola', '0000-00-00 00:00:00', '2015-06-04 15:24:46', '', 4),
-(36, 2, 'labobo', '0000-00-00 00:00:00', '2015-06-04 15:24:46', '', 4);
+INSERT INTO `act` (`id`, `u_id`, `name`, `j_time`, `s_time`, `extra`, `t_id`, `a_state`) VALUES
+(1, 1, 'running', '2015-05-04 12:00:00', '2015-08-04 08:42:55', 'everyone is ok', 1, '进行中'),
+(2, 1, 'running2', '2015-05-04 12:00:00', '2015-08-04 08:43:12', 'everyone is ok', 1, '进行中'),
+(3, 1, 'running23', '2015-05-04 12:00:00', '2015-08-04 08:43:16', 'everyone is ok', 1, '进行中'),
+(4, 1, 'running234', '2015-05-04 12:00:00', '2015-08-04 08:43:18', 'everyone is ok', 1, '进行中'),
+(5, 1, 'running2345', '2015-05-04 12:00:00', '2015-08-04 08:43:25', 'everyone is ok', 1, '报名中'),
+(6, 1, 'running23456', '2015-05-04 12:00:00', '2015-08-04 08:43:30', 'everyone is ok', 1, '报名中'),
+(7, 1, 'running234567', '2015-05-04 12:00:00', '2015-08-04 08:43:39', 'everyone is ok', 2, '已结束'),
+(8, 1, 'running2345678', '2015-05-04 12:00:00', '2015-08-04 08:43:43', 'everyone is ok', 2, '已结束'),
+(9, 1, 'running23456789', '2015-05-04 12:00:00', '2015-08-04 08:51:27', 'everyone is ok', 3, '已结束'),
+(10, 1, 'running234567890', '2015-05-04 12:00:00', '2015-08-04 11:39:47', 'everyone is ok', 7, '进行中'),
+(11, 1, 'wtf', '2015-05-04 12:00:00', '2015-08-04 11:39:49', 'everyone is ok', 1, '进行中'),
+(12, 1, 'wtf1', '2015-05-04 12:00:00', '2015-08-04 11:39:51', 'everyone is ok', 1, '进行中'),
+(13, 1, 'wtf12', '2015-05-04 12:00:00', '2015-08-04 11:39:54', 'everyone is ok', 5, '进行中'),
+(14, 1, 'wtf123', '2015-05-04 12:00:00', '2015-08-04 11:39:56', 'everyone is ok', 1, '进行中'),
+(15, 1, 'wtf1234', '2015-05-04 12:00:00', '2015-08-04 11:39:57', 'everyone is ok', 1, '进行中'),
+(16, 1, 'wtf12345', '2015-05-04 12:00:00', '2015-08-04 11:40:01', 'everyone is ok', 1, '进行中'),
+(17, 2, 'study', '0000-00-00 00:00:00', '2015-08-04 12:04:16', 'everyone is ok', 7, '进行中'),
+(18, 2, 'studyy', '0000-00-00 00:00:00', '2015-08-04 12:04:15', 'everyone is ok', 7, '进行中'),
+(19, 2, 'studyyy', '0000-00-00 00:00:00', '2015-08-04 12:04:19', 'everyone is ok', 7, '进行中'),
+(20, 2, 'studyyyy', '0000-00-00 00:00:00', '2015-08-04 12:04:18', 'everyone is ok', 7, '进行中'),
+(21, 2, 'studyyyyy', '0000-00-00 00:00:00', '2015-08-04 12:04:20', 'everyone is ok', 7, '进行中'),
+(22, 2, 'studyyyyyy', '0000-00-00 00:00:00', '2015-08-04 12:04:21', 'everyone is ok', 7, '进行中'),
+(23, 2, 'studys', '0000-00-00 00:00:00', '2015-08-04 12:04:23', 'everyone is ok', 7, '进行中'),
+(24, 2, 'studyss', '0000-00-00 00:00:00', '2015-08-04 12:04:25', 'everyone is ok', 7, '进行中'),
+(25, 2, 'studysss', '0000-00-00 00:00:00', '2015-08-04 12:04:12', 'everyone is ok', 7, '进行中'),
+(26, 2, 'studyssss', '0000-00-00 00:00:00', '2015-08-04 12:04:13', 'everyone is ok', 7, '进行中'),
+(27, 2, 'studysssss', '0000-00-00 00:00:00', '2015-08-04 12:04:11', 'everyone is ok', 7, '进行中'),
+(28, 2, 'studyssssss', '0000-00-00 00:00:00', '2015-08-04 12:04:08', 'everyone is ok', 7, '进行中'),
+(29, 2, 'study1', '0000-00-00 00:00:00', '2015-08-04 12:04:09', 'everyone is ok', 7, '进行中'),
+(30, 2, 'study11', '0000-00-00 00:00:00', '2015-08-04 12:04:06', 'everyone is ok', 7, '进行中'),
+(31, 2, 'study111', '0000-00-00 00:00:00', '2015-08-04 12:04:04', 'everyone is ok', 7, '进行中'),
+(32, 2, 'study1111', '0000-00-00 00:00:00', '2015-08-04 12:04:03', 'everyone is ok', 7, '进行中'),
+(33, 2, 'lalala', '0000-00-00 00:00:00', '2015-08-04 12:04:02', 'everyone is ok', 2, '进行中'),
+(34, 2, 'bobobo', '0000-00-00 00:00:00', '2015-08-04 12:04:00', 'everyone is ok', 3, '进行中'),
+(35, 2, 'labola', '0000-00-00 00:00:00', '2015-08-04 12:03:59', 'everyone is ok', 4, '进行中'),
+(36, 2, 'labobo', '0000-00-00 00:00:00', '2015-08-04 12:03:57', 'everyone is ok', 4, '进行中');
 
 -- --------------------------------------------------------
 
@@ -94,6 +95,28 @@ CREATE TABLE IF NOT EXISTS `act_man` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `follow`
+--
+
+CREATE TABLE IF NOT EXISTS `follow` (
+  `f_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `follower_id` bigint(11) NOT NULL,
+  `followed_id` bigint(11) NOT NULL,
+  PRIMARY KEY (`f_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- 转存表中的数据 `follow`
+--
+
+INSERT INTO `follow` (`f_id`, `follower_id`, `followed_id`) VALUES
+(1, 11, 4),
+(2, 11, 6),
+(3, 11, 7);
 
 -- --------------------------------------------------------
 
@@ -161,7 +184,7 @@ CREATE TABLE IF NOT EXISTS `reg` (
 
 CREATE TABLE IF NOT EXISTS `type` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+  `t_name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
@@ -169,7 +192,7 @@ CREATE TABLE IF NOT EXISTS `type` (
 -- 转存表中的数据 `type`
 --
 
-INSERT INTO `type` (`id`, `name`) VALUES
+INSERT INTO `type` (`id`, `t_name`) VALUES
 (0, '全部活动'),
 (1, '跑步'),
 (2, '拼车'),
@@ -237,7 +260,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `sign` varchar(255) NOT NULL,
   `pvg` int(1) NOT NULL DEFAULT '5',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
 
 --
 -- 转存表中的数据 `user`
@@ -253,7 +276,8 @@ INSERT INTO `user` (`id`, `email`, `phone`, `pwd`, `name`, `nname`, `birth`, `ho
 (7, 'youxia@126.com', '13615468544', '000000', 'guestguestgue', 'guestguestgue', '2010-09-16', '湖南长沙', '上海交通大学', '电院', '信安', '2015-07-13', NULL, NULL, NULL, NULL, NULL, '你猜呀', '继续猜啊', '', '低调', 5),
 (8, 'youxi@126.com', '13615468542', '000000', 'guestguestg', 'guestguestg', '2010-09-16', '湖南长沙', '上海交通大学', '电院', '信安', '2015-07-13', NULL, NULL, NULL, NULL, NULL, '你猜呀', '继续猜啊', '', '低调', 5),
 (9, 'youx@126.com', '13615468541', '000000', 'guestguest', 'guestguest', '2010-09-16', '湖南长沙', '上海交通大学', '电院', '信安', '2015-07-13', NULL, NULL, NULL, NULL, NULL, '你猜呀', '继续猜啊', '', '低调', 5),
-(10, 'you@126.com', '13615468540', '000000', 'guestgues', 'guestgues', '2010-09-16', '湖南长沙', '上海交通大学', '电院', '信安', '2015-07-13', NULL, NULL, NULL, NULL, NULL, '你猜呀', '继续猜啊', '', '低调', 5);
+(10, 'you@126.com', '13615468540', '000000', 'guestgues', 'guestgues', '2010-09-16', '湖南长沙', '上海交通大学', '电院', '信安', '2015-07-13', NULL, NULL, NULL, NULL, NULL, '你猜呀', '继续猜啊', '', '低调', 5),
+(11, 'caozhechao@126.com', '', '*6BB4837EB74329105EE4568DDA7DC67ED2CA2AD9', '', '土拨鼠1313', '0000-00-00', '上海闸北', 'sjtu', '', '电院信安', '0000-00-00', NULL, NULL, NULL, NULL, NULL, 'singledog', '。', '', '给清昊找妹子5', 5);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
